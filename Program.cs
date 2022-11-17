18420ca4ed92b903690d2109d4376b0e7a848a3a
karol <karol@CARLOSOFFX>
2022-11-17 22:54:34 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel;
+using System.Net.Http.Headers;
+using System.Reflection;
+
+class Program
+{
+    public static void Main()
+    {
+        string input = Console.ReadLine();
+        double[] X = Array.ConvertAll<string, double>(input.Split(" "), double.Parse);
+        double a = X[0];
+        double b = X[1];
+        double min = -32.768;
+        double max = 32.767;
+
+
+
+        for (double i = a; i < b -1; i++) Console.Write($"{i+1} ");
+
+        /*if (a == b || a + 1 > b || a - 1 < b || b + 1 > a || b - 1 < a)
+        {
+            Console.WriteLine("Empty");
+        }
+        */
+
+        for (double j = a; j >= 11; j++) Console.WriteLine($"{j+1},{j+2},{j+3}, ..., {b-2}{b-1} ");
+        
+
+
+
+
+
+
+
+
+
+    }
+}
