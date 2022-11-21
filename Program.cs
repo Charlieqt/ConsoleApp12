46e4cdf89496d67a3b1c8bd3d72396aefe0aabc6
Charlieqt <115217385+Charlieqt@users.noreply.github.com>
2022-11-21 23:49:20 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -13,37 +13,53 @@ class Program
         double b = X[1];
         double min = -32.768;
         double max = 32.767;
+
+
+
+
         
 
-
-
-        //for (double i = a; i < b - 1; i++) Console.Write($"{i + 1} ");
-
-        /*if (a == b || a + 1 > b || a - 1 < b || b + 1 > a || b - 1 < a)
+        
+       for (double i = a; i < b - 1; i++)
+        {
+            Console.Write($"{i + 1} ");
+            
+        }
+        /*(a == b || a + 1 > b || a - 1 < b || b + 1 > a || b - 1 < a)
         {
             Console.WriteLine("Empty");
         }
+        /*if (b - a <= 11 && a + b >= 11)
+         {
+             if (a < b)
+             {
+
+
+
+                 /*for (double i = a; i < b - 1; i++)
+                 {
+                     Console.Write($"{i + 1} ");
+                 }
+                 */
+        /*for (double j = a; j <= 11; j++)
+        {
+
+            Console.WriteLine($"{j + 1},{j + 2},{j + 3}, ..., {b - 2},{b - 1}");
+            break;
+        }
         */
 
-        //for (double j = a; j+b >= 11; j++) Console.WriteLine($"{j + 1},{j + 2},{j + 3}, ..., {b - 2}{b - 1} ");
-
-        if (b - a <= 11 && a + b >= 11)
-        {
-           if(a<b)
-            {
-                for (double j = a; j <= 11; j++)
-                {
-
-                    Console.WriteLine($"{j + 1},{j + 2},{j + 3}, ..., {b - 2},{b - 1}");
-                   
-
-                }
 
 
 
 
-            }
-        }
+
+
+
+
+        // }
+
+        //}
 
 
 
